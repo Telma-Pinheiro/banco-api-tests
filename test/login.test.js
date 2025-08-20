40d02325290a39de12822f7c3b0a1d0b1f003de6
Telma-Pinheiro <telmacarv12@gmail.com>
2025-08-20 16:32:37 -0300

    Iniciando o projeto

---
+++ test/login.test.js
@@ -0,0 +1,26 @@
+const request = require('supertest');
+const { expect } = require('chai')
+
+describe('login', () => {
+    describe('POST /login', () => {
+        it('Deve retornar 200 com um token em string quando usar credenciais válidas', async () => {
+            const response = await request('http://localhost:3000')
+            //vou fazer uma requisição via post com credenciasi válidas para o post login
+                .post('/login')
+                //setando o cabeçalho pra essa requisição
+                .set('Content-Type', 'application/json')
+                .send({
+                    "username": 'julio.lima',
+                    "senha": '123456'
+                })
+                // agora vou fazer as validações com chai(importei ali em cima
+                //espero q a resposta
+                expect(response.status).to.equal(200);
+                expect(response.body.token).to.be.a('string');
+                
+
+
+         })
+            
+    })
+})
