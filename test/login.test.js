0f0e0fb7b22c876fd1283d961dfb3fcda605df48
Telma-Pinheiro <telmacarv12@gmail.com>
2025-08-25 15:20:26 -0300

    refactor: renomear variável response para resposta no login

--- test/login.test.js
+++ test/login.test.js
@@ -4,19 +4,19 @@ const { expect } = require('chai')
 describe('login', () => {
     describe('POST /login', () => {
         it('Deve retornar 200 com um token em string quando usar credenciais válidas', async () => {
-            const response = await request('http://localhost:3000')
+            const resposta = await request('http://localhost:3000')
             //vou fazer uma requisição via post com credenciasi válidas para o post login
                 .post('/login')
                 //setando o cabeçalho pra essa requisição
                 .set('Content-Type', 'application/json')
                 .send({
-                    "username": 'julio.lima',
-                    "senha": '123456'
+                    'username': 'julio.lima',
+                    'senha': '123456'
                 })
                 // agora vou fazer as validações com chai(importei ali em cima
                 //espero q a resposta
-                expect(response.status).to.equal(200);
-                expect(response.body.token).to.be.a('string');
+                expect(resposta.status).to.equal(200);
+                expect(resposta.body.token).to.be.a('string');
                 
 
 
